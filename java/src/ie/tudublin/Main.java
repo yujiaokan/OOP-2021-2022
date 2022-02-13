7041bc5e884ab6757c25ea0085d849c8ac5dbba0
yujiao <D20125727@mytudublin.ie>
2022-02-13 22:52:53 +0000

    kill cat

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,43 +2,36 @@ package ie.tudublin;
 
 public class Main
 {
-    public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
     
 
-    public void cat()
-    {
-        System.out.println("Hello world");
-
-        Animal misty = new Animal("Misty");
-        Animal lucy = new Animal("Lucy");
-
-        lucy = misty;
-        misty.setName("Tara");
-
-        System.out.println(misty);
-        System.out.println(lucy);
-
-        Cat cat = new Cat("Ginger");
-
-        while(cat.getNumLives() > 0)
-        {
-            cat.kill();
-        }
-        cat.kill();
-
-    }
+    
     public static void main(String[] args)
     {
-        helloProcessing();
         
+            System.out.println("Hello world");
 
+            Animal misty = new Animal("Misty");
+            Animal lucy = new Animal("Lucy");
+
+            lucy = misty;
+            misty.setName("Tara");
+
+            System.out.println(misty);
+            System.out.println(lucy);
+
+            Cat cat = new Cat("Ginger");
+
+            while(cat.getNumLives() > 0)
+            {
+                cat.kill();
+            }
+            cat.kill();
+
+        
+        
+        
+    }    
         // Tara Misty
         // Tara Tara
         
-    }
 }
